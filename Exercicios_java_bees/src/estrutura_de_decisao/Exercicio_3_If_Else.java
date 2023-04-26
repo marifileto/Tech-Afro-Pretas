f9b719b2b71eb0864fab067a40ea2e0d05bb2c88
Marilia <mariliafileto.mf@gmail.com>
2023-04-26 14:53:13 -0300

    Ajustes dos nomes das pastas

---
+++ Exercicios_java_bees/src/estrutura_de_decisao/Exercicio_3_If_Else.java
@@ -0,0 +1,41 @@
+package estrutura_de_decisao;
+
+import java.util.Scanner;
+
+public class Exercicio_3_If_Else {
+	
+	/*Faça um programa que receba a idade de uma pessoa e mostre na saída em qual
+	categoria ela se encontra:
+	10-14 infantil
+	15-17 juvenil
+	18-25 adulto*/
+	
+	public static void main(String[] args) {
+
+		Scanner leia = new Scanner(System.in);
+
+		float y, infantil, juvenil, adulto;
+		int idade;
+		
+		System.out.println("Digite a idade de uma pessoa: ");
+		idade = leia.nextInt();
+		
+							
+			if ((idade <= 14) && (idade >= 10)) {
+				System.out.println("Esta pessoa se encaixa na categoria infantil.");
+			}
+			
+			else if ((idade <= 17) &&(idade >= 15)) {
+				System.out.println("Esta pessoa se encaixa na categoria juvenil.");
+			}
+			
+			else if ((idade <= 25) && (idade >= 18)) {
+				System.out.println("Esta pessoa se encaixa na categoria adulto.");
+			}
+			
+			else {
+				System.out.println("Esta pessoa não se encaixa em nenhuma categoria.");
+			}
+	}
+
+}
---
+++ Exercicios_java_bees/src/estrutura_de_repeticao/Exercicio_4_repet_while.java
@@ -0,0 +1,39 @@
+package estrutura_de_repeticao;
+
+import java.util.Scanner;
+
+public class Exercicio_4_repet_while {
+	
+	/*Obtenha um número digitado pelo usuário e repita a operação de multiplicar ele por três  
+	(imprimindo o novo valor) até que ele seja maior do que 100. Ex.: se o usuário digita 5, 
+	deveremos observar na tela a seguinte sequência: 5 15 45 135.*/
+
+	public static void main(String[] args) {
+		
+		Scanner leia = new Scanner(System.in);
+		
+		
+		//declarando as variáveis que receberão valores
+		int y = 0, cont = 0;
+
+		//solicita na tela que o valor seja digitado
+		System.out.println("Digite um número até 100: ");
+		//atribui o valor digitado à variável "y"
+		y = leia.nextInt();
+
+		//inicializa a contagem com o valor digitado pelo usuário
+		cont = y;
+
+		//imprime o valor de y na tela
+		System.out.println(" " + cont);
+
+		//iniciando o loop para o cálculo solicitado
+		while (cont <= 100){
+					
+			cont = cont * 3;
+			//imprime os resultados na tela
+			System.out.println(" " + cont);
+		}
+	}
+
+}
